123a5db3b221da710bce6800460302e6588db523
Ezra Developer <ibxbit@gmail.com>
2025-07-22 20:02:33 +0300

    comment routes added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,11 @@
 import express from "express";
 import cors from "cors";
 import {clerkMiddleware} from "@clerk/express";
+
 import userRoutes from "./routes/user.route.js";
 import postRoutes from "./routes/post.route.js";
+
+import commentRoutes from "./routes/comment.route.js";
 
 import { ENV } from "./config/env.js";
 import connectDB from "./config/db.js";
@@ -20,6 +23,7 @@ app.get("/", (req, res) => res.send("Hello from server"));
 
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
+app.use("/api/comments", commentRoutes);
 
 // Error handling middleware
 app.use((err, req, res, next) => {
