9f4dd476c5ab5fadf43c0efe9862fbb5c36b0808
Ezra Developer <ibxbit@gmail.com>
2025-07-23 11:14:54 +0300

    implementation of auth

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,14 +1,15 @@
-import { Text, View } from 'react-native';
+import { useClerk } from '@clerk/clerk-expo'
+import { View, Text, Button } from 'react-native'
 
+const HomeScreen = () => {
+    const { signOut }=useClerk
+    return (
+        <View>
+            <Text>HomeScreen</Text>
 
-export default function Index() {
-  return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-red-500">
-        Welcome to Nativewind!
-      </Text>
-    </View>
-  );
+            <Button onPress={() => signOut()} title='logout'></Button>
+        </View>
+    )
 }
 
-
+export default HomeScreen
